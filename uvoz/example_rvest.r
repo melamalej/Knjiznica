72a300f9adacb07fcbdb3cf4387c35c030715a37
melamalej <mela.malej@gmail.com>
2020-05-07 13:33:03 +0200

    Popravile tabelo books.

--- uvoz/example_rvest.r
+++ uvoz/example_rvest.r
@@ -5,19 +5,33 @@ library(gsubfn)
 library(readr)
 
 # Naslov, od koder pobiramo podatke
-link <- "https://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-stran <- html_session(link) %>% read_html()
+#link <- "https://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+#stran <- html_session(link) %>% read_html()
 
 # Preberemo prvo ustrezno tabelo
-tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-  .[[1]] %>% html_table()
+#tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+  #.[[1]] %>% html_table()
 
-# Nadomestimo decimalne vejice in ločila tisočic ter pretvorimo v števila
-sl <- locale(decimal_mark=",", grouping_mark=".")
-for (i in c(2, 3, 5, 6)) {
-  tabela[[i]] <- tabela[[i]] %>% parse_number(na="-", locale=sl)
-}
-tabela[[9]] <- tabela[[9]] %>% parse_character(na="-")
+knjige <- read_csv("knjige.csv")
+knjige$Height <- NULL
+knjige$SubGenre <- NULL
+knjige$Publisher <- NULL
+newdata <- na.omit(knjige)  
+
+v <- round(runif(187, min = 10000, max = 99999))    
+
+newdata$KobissID <- v
+newdata$Availability <- rep(c("yes"), times = 187)
 
 # Zapišemo v datoteko CSV
-write_csv(tabela, "obcine.csv", na="")
+write_csv(newdata, "books.csv", na="")
+
+# Nadomestimo decimalne vejice in ločila tisočic ter pretvorimo v števila
+#sl <- locale(decimal_mark=",", grouping_mark=".")
+#for (i in c(2, 3, 5, 6)) {
+  #tabela[[i]] <- tabela[[i]] %>% parse_number(na="-", locale=sl)
+#}
+#tabela[[9]] <- tabela[[9]] %>% parse_character(na="-")
+
+# Zapišemo v datoteko CSV
+#write_csv(tabela, "obcine.csv", na="")
