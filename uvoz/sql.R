adaff8efe2be0b99fc51ea51edb08770bcb69be7
melamalej <mela.malej@gmail.com>
2020-05-08 11:15:45 +0200

    Krneki

--- uvoz/sql.R
+++ uvoz/sql.R
@@ -7,33 +7,34 @@ source("uvoz/auth.R")
 
 # Povežemo se z gonilnikom za PostgreSQL
 drv <- dbDriver("PostgreSQL")
-blablabla
 # Uporabimo tryCatch,
 # da prisilimo prekinitev povezave v primeru napake
 tryCatch({
   # Vzpostavimo povezavo
   conn <- dbConnect(drv, dbname=db, host=host, user=user, password=password)
-  
+  t <- dbGetQuery(conn, build_sql("SELECT * FROM books", con=conn))
+                   
   # Poizvedbo zgradimo s funkcijo build_sql
   # in izvedemo s funkcijo dbGetQuery
-  znesek <- 1000
-  t <- dbGetQuery(conn, build_sql("SELECT * FROM transakcija
-                                  WHERE znesek >", znesek, "
-                                  ORDER BY znesek, id", con=conn))
+  #znesek <- 1000
+  #t <- dbGetQuery(conn, build_sql("SELECT * FROM transakcija
+                                  #WHERE znesek >", znesek, "
+                                  #ORDER BY znesek, id", con=conn))
   # Rezultat dobimo kot razpredelnico (data frame)
   
+  
   # Vstavimo še eno transakcijo
-  i <- round(runif(1, 1, nrow(t)))
-  print("Storniramo transkacijo:")
-  print(t[i,])
-  znesek <- -t[i, "znesek"]
-  racun <- t[i, "racun"]
-  opis <- paste("Storno:", t[i, "opis"])
+  #i <- round(runif(1, 1, nrow(t)))
+  #print("Storniramo transkacijo:")
+  #print(t[i,])
+  #znesek <- -t[i, "znesek"]
+  #racun <- t[i, "racun"]
+  #opis <- paste("Storno:", t[i, "opis"])
   
   # Pošljemo poizvedbo
-  dbSendQuery(conn, build_sql("INSERT INTO transakcija (znesek, racun, opis)
-                               VALUES (", znesek, ", ", racun, ", ", opis, ")", con=conn))
-  }, finally = {
+  #dbSendQuery(conn, build_sql("INSERT INTO transakcija (znesek, racun, opis)
+                               #VALUES (", znesek, ", ", racun, ", ", opis, ")", con=conn))
+  #}, finally = {
     # Na koncu nujno prekinemo povezavo z bazo,
     # saj preveč odprtih povezav ne smemo imeti
     dbDisconnect(conn)
