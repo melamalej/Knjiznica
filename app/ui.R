8133cd48c06490e8ba3b53e8ca963f98762a91ee
Lana Zajc <lana.zajc@gmail.com>
2020-05-12 10:24:44 +0200

    Dodala še možnost vpisa za zaposlene in člane

--- app/ui.R
+++ app/ui.R
@@ -21,14 +21,20 @@ shinyUI(dashboardPage(
       # Second tab content
       tabItem(tabName = "knjige",
               h2("Search by title or by author"), box('', textInput('text', 'Title'), textInput('text', 'Author'),
-                                             actionButton('isci', 'Search')
-            )),
+                                             actionButton('isci', 'Search'))
+            ),
       # Third tab content2
       tabItem(tabName = "izposoja",
-              h2("Widgets tab content")),
+              h2("Widgets tab content"), box('Vpis_zaposleni', textInput('text', 'User name'), textInput('text', 'Password'),
+                                             actionButton('vpis', 'Login'))), 
+      #tuki bi lahko mele možnost, da se vpiše samo knjižničarka, ker sam sebi itak ne morš izposodit knjige
+      
       # Fourth tab content
       tabItem(tabName = "profil",
-              h2("Widgets tab content"))
+              h2("Widgets tab content"), box('Vpis_clan', textInput('text', 'User name'), textInput('text', 'Password'),
+                                             actionButton('vpis', 'Login')))
+      
+      # tuki bi mele pa možnost da se vpiše študent in pol vidi naprej svoj profil..Ko pritisneš login bi se ti desno izpisali podatki
     )
   )
 )
