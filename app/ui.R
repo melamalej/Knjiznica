83c5d5cf5250a19c40c48575d7bbc58bb5c98915
melamalej <mela.malej@gmail.com>
2020-05-14 16:21:27 +0200

    Postavitev strani.

--- app/ui.R
+++ app/ui.R
@@ -1,23 +1,40 @@
 library(shiny)
 library(shinydashboard)
 
+#signin <- tabPanel("SignIn", value="signIn",
+                        #fluidPage( 
+                          #titlePanel("Welcome to our library. Please sign in."),
+                          #img(src = "naslovna.jpg", height = 140, width = 400),
+                          #fluidRow(
+                            #column(width = 12,
+                                   #align = "center",
+                                   #textInput("userName","User name", value= "", placeholder = "User name"),
+                                   #passwordInput("password","Password", value = "", placeholder = "Password"),
+                                   #actionButton("signin_btn", "Sign In")
+                            #))
+                        #)
+#)
 
-vpisniPanel <- tabPanel("SignIn", value="signIn",
-                        fluidPage( titlePanel("Welcome to or library. Please sign in."),
-                          #HTML('<body background = "https://raw.githubusercontent.com/ZavbiA/Iskalnik-postnih-posiljk/master/slike/digital-mail-2-1.jpg"></body>'),
-                          fluidRow(
-                            column(width = 12,
-                                   align = "center",
-                                   textInput("userName","User name", value= "", placeholder = "User name"),
-                                   passwordInput("password","Password", value = "", placeholder = "Password"),
-                                   actionButton("signin_btn", "Sign In")
-                            )
+shinyUI(navbarPage("Library",
+                 tabPanel("Books"),
+                 navbarMenu("Browse",
+                            tabPanel("By title",
+                                     textInput("text", h3("Enter title"), 
+                                               value = "Enter text...")),
                             
-                            
-                          )))
-
-
-
+                            tabPanel("By author",
+                                     textInput("text", h3("Enter author"), 
+                                               value = "Enter text...")),
+                            tabPanel("By genre",
+                                     textInput("text", h3("Enter genre"), 
+                                               value = "Enter text..."))
+                          ),
+          
+                 navbarMenu("My profile",
+                            tabPanel("My loans"),
+                            tabPanel("Borrow"),
+                            tabPanel("Return"))
+))
 
 
 #shinyUI(fluidPage(
@@ -38,3 +55,4 @@ vpisniPanel <- tabPanel("SignIn", value="signIn",
     #)
   #)
 #))
+
